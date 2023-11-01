0eeed2d673c8fbdbbe02f7fa2e87e6e7abbcc281
Nicolas Torres <nicolastorres@mailbox.org>
2023-11-01 16:20:11 +0100

    feat: separate css and favicon

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <Adafruit_NeoPixel.h>
 #include <ESPAsyncWebServer.h>
 #include <WiFi.h>
+#include <SPIFFS.h>
 #include "credentials.h"
 
 #define PIN_LEDS 16
@@ -45,15 +46,7 @@ String constructHtml(bool ledState) {
     String ptr = "<!DOCTYPE html> <html>\n";
     ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
     ptr += "<title>LED Control</title>\n";
-    ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-    ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
-    ptr += ".button {display: block;width: 80px;background-color: #3498db;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
-    ptr += ".button-on {background-color: #3498db;}\n";
-    ptr += ".button-on:active {background-color: #2980b9;}\n";
-    ptr += ".button-off {background-color: #34495e;}\n";
-    ptr += ".button-off:active {background-color: #2c3e50;}\n";
-    ptr += "p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
-    ptr += "</style>\n";
+    ptr += R"(<link rel="stylesheet" type="text/css" href="/style.css">)";
     ptr += "</head>\n";
     ptr += "<body>\n";
     ptr += "<h1>ESP32 Web Server</h1>\n";
@@ -93,6 +86,7 @@ void handleOff(AsyncWebServerRequest *request) {
 
 void setup() {
     leds.begin();
+    SPIFFS.begin(true);
     Serial.begin(115200);
     Serial.print("Connecting to...");
     Serial.print(ssid);
@@ -107,6 +101,12 @@ void setup() {
     Serial.println(WiFi.localIP());
 
     server.on("/", HTTP_GET, serveSite);
+    server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(SPIFFS, "/style.css", "text/css");
+    });
+    server.on("/index", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(SPIFFS, "/index.html", "text/html");
+    });
     server.on("/on", HTTP_GET, handleOn);
     server.on("/off", HTTP_GET, handleOff);
     server.begin();
